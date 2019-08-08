55a82f8b7310c4e6b6c01cba4a9d1d86d6aa40ba
ljzgbtc <53665957+ljzgbtc@users.noreply.github.com>
2019-08-07 17:28:22 -0700

    Add project

---
+++ projects/HelloWTP/js/jshint_example.js
@@ -0,0 +1,23 @@
+
+
+
+function thisIsNeverUsed(){
+	 if (1 == 0)
+	  console.log(" 1 is never equal 0")
+	  switch (something) {
+	  case 1:
+	   console.log("it is 1");
+	  default:
+	   break;
+	  }
+	 return true;
+	 var somethingElse = 3;
+}
+
+function getName(){
+	var fn = "John";
+	var ln = "Smith";
+	var cn = ln + ", " + fn;
+	return cn;
+}
+
